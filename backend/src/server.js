181d7788b934ab133111036a51084f470b7a7e74
MrPravinS <shegamwarpravin07@gmail.com>
2025-09-13 17:25:54 +0530

    initiate frontend and backend

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+
+const app = express();
+const PORT = 3000
+
+app.get("/",(req,res)=>{
+  res.json({
+    message:"This is get request"
+  })
+})
+
+
+app.listen(PORT,()=>{
+    console.log("Server is running on PORT ",PORT);
+    
+})
